8f740e5cfacdd49032fa940a55c1cc9223b896e3
Lloyd <lloyd@pikicast.com>
2016-02-10 21:38:33 +0900

    edit root response

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send({ message: 'OK' });
+  res.send({ message: 'api server' });
 });
 
 app.listen(port, () => console.log(util.format('Listen %s', port)));
